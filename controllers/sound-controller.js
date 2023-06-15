7bef7546bcb6e7fbe26d633eaa756b8082d21b11
Joshua Sinkinson <joshuasinkinson93@gmail.com>
2023-06-15 22:16:40 +0100

    Test query parameters

--- controllers/sound-controller.js
+++ controllers/sound-controller.js
@@ -1,9 +1,16 @@
 const knex = require("knex")(require("../knexfile"));
 
 // All sounds in database
-const allSounds = async (_req, res) => {
+const allSounds = async (req, res) => {
+  const { type, subgenre, key_scale } = req.query;
   try {
-    const sounds = await knex("sounds").select();
+    const sounds = await knex("sounds")
+      .select()
+      .where({
+        type,
+        subgenre,
+        key_scale,
+      });
     res.status(200).json(sounds);
   } catch (error) {
     res.status(500).json({
