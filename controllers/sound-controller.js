92ca618297bb82b4da941544ada9ecbe672869cb
Joshua Sinkinson <joshuasinkinson93@gmail.com>
2023-06-20 22:38:53 +0100

    Add conditional logic to sound controller & reseed sound database

--- controllers/sound-controller.js
+++ controllers/sound-controller.js
@@ -1,23 +1,69 @@
 const knex = require("knex")(require("../knexfile"));
 
-// All sounds in database
+// Checks if an object is empty
+const isObjectEmpty = (obj) => {
+  return Object.keys(obj).length === 0;
+};
+
+// Checks if an object has two keys
+const isObjectTwo = (obj) => {
+  return Object.keys(obj).length === 2;
+};
+
+// Checks if req.query is empty, if true all sounds are provided
+// If not, the query parameters are applied
 const allSounds = async (req, res) => {
-  const { type, subgenre, key_scale } = req.query;
-  try {
-    const sounds = await knex("sounds")
-      .select()
-      .where({
+  if (isObjectEmpty(req.query)) {
+
+    try {
+      const sounds = await knex("sounds").select();
+      res.status(200).json(sounds);
+    } catch (error) {
+      res.status(500).json({
+        error: true,
+        message: `Error retriving sounds`,
+        detail: `${error.message}`,
+      });
+    }
+
+  } else if (isObjectTwo(req.query)) {
+
+    const { type, subgenre } = req.query;
+
+    try {
+      const sounds = await knex("sounds").select().where({
+        type,
+        subgenre,
+      });
+      res.status(200).json(sounds);
+    } catch (error) {
+      res.status(500).json({
+        error: true,
+        message: `Error retriving sounds`,
+        detail: `${error.message}`,
+      });
+    }
+
+  } else {
+
+    const { type, subgenre, key_scale, rel_key_scale } = req.query;
+
+    try {
+      const sounds = await knex("sounds").select().where({
         type,
         subgenre,
         key_scale,
+        rel_key_scale,
       });
-    res.status(200).json(sounds);
-  } catch (error) {
-    res.status(500).json({
-      error: true,
-      message: `Error retriving sounds`,
-      detail: `${error.message}`,
-    });
+      res.status(200).json(sounds);
+    } catch (error) {
+      res.status(500).json({
+        error: true,
+        message: `Error retriving sounds`,
+        detail: `${error.message}`,
+      });
+    }
+
   }
 };
 
